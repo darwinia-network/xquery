e6db2f7ccbd4fdbf5f14abfe4a74e25023b8b708
wuminzhe <wuminzhe@gmail.com>
2022-08-16 11:05:55 +0800

    second commit

--- index.ts
+++ index.ts
@@ -1,19 +1,37 @@
+// typegraphql
+import "reflect-metadata";
+import { resolvers } from "@generated/type-graphql"
+import { buildSchema } from 'type-graphql'
+// Prisma
 import { PrismaClient } from '@prisma/client'
-
 const prisma = new PrismaClient()
+// appllo
+const { ApolloServer } = require('apollo-server')
+
 
 async function main() {
-  // ... you will write your Prisma Client queries here
-  const allUsers = await prisma.user.findMany()
-  console.log(allUsers)
+    const schema = await buildSchema({
+        resolvers,
+        validate: false,
+    });
+
+    const server = new ApolloServer({
+        schema,
+        context: () => ({ prisma }),
+    });
+    
+    server.listen().then(() => {
+        console.log(`🚀  Server ready at http://localhost:4000/`);
+    });
+    
 }
 
 main()
-  .then(async () => {
-    await prisma.$disconnect()
-  })
-  .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
+    .then(async () => {
+        await prisma.$disconnect()
+    })
+    .catch(async (e) => {
+        console.error(e)
+        await prisma.$disconnect()
+        process.exit(1)
+    })
